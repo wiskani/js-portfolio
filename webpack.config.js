aa0337391f009889e684adaa99dfa4d975e95891
wiskani <wiskani@icloud.com>
2022-05-30 21:57:37 -0400

    empaquetando imagenes css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path= require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry:'./src/index.js',
@@ -18,6 +20,17 @@ module.exports = {
 				use: {
 					loader: 'babel-loader'
 				}
+			},
+			{
+				test: /\.css|.styl$/i,
+				use: [MiniCssExtractPlugin.loader,
+					'css-loader',
+					'stylus-loader'
+				],
+			},
+			{
+				test: /\.png/,
+				type: 'asset/resource'
 			}
 		]
 	},
@@ -26,6 +39,15 @@ module.exports = {
 			inject: true,
 			template: './public/index.html',
 			filename: 'index.html'
+		}),
+		new MiniCssExtractPlugin(),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname,"src", "assets/images"),
+					to: "assets/images"
+				}
+			]
 		})
 	]
 }
