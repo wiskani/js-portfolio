c0c15dfd1cc64cfcc4963916b588d4e15f73b82f
wiskani <wiskani@icloud.com>
2022-05-31 06:54:21 -0400

    hashes y comprencion

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path= require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
 	entry:'./src/index.js',
@@ -40,7 +42,7 @@ module.exports = {
 						options: {
 							limit:10000,
 							mimetype: "application/font-woff",
-							name: "[name].[ext]",
+							name: "[name].[contenthash],[ext]",
 							outputPath: "./assets/fonts/",
 							publicPath:"./assets/fonts",
 							esModule: false,
@@ -55,7 +57,9 @@ module.exports = {
 			template: './public/index.html',
 			filename: 'index.html'
 		}),
-		new MiniCssExtractPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'assets/[name].[contenthash].css'
+		}),
 		new CopyPlugin({
 			patterns: [
 				{
@@ -64,5 +68,12 @@ module.exports = {
 				}
 			]
 		})
-	]
+	],
+	optimization:{
+		minimize: true,
+		minimizer: [
+			new CssMinimizerPlugin(),
+			new TerserPlugin(),
+		]
+	}
 }
